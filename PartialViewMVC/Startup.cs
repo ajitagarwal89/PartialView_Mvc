f49ddc820e6a79e8784dca3ef39365f618efddf0
ajitagarwal89 <ajitagarwal89@outlook.com>
2017-03-01 23:51:31 +0530

    Add project files.

---
+++ PartialViewMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PartialViewMVC.Startup))]
+namespace PartialViewMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
